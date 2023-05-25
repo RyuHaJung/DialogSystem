4ef08ab0f2e922f2f27082a8ad04a3028d61c06d
RyuHaJung <red061011@gmail.com>
2023-05-25 11:57:51 +0900

    다이얼로그 테스트 및 씬 이동시 대사 나오는 스크립트, 등장인물이 안나오는 스크립트 추가

--- Assets/Scripts/DialogTest.cs
+++ Assets/Scripts/DialogTest.cs
@@ -6,9 +6,11 @@ public class DialogTest : MonoBehaviour
 {
     [SerializeField]
     private DialogSystem dialogSystem;
+    public int Dialogindex = 0;
+
     private IEnumerator Start()
     {
-        yield return new WaitUntil(() => dialogSystem.UpdateDialog(0, true)); //기다리는 함수 , 다이얼로그 시스템이 완료 될때 까지 
+        yield return new WaitUntil(() => dialogSystem.UpdateDialog(Dialogindex, true)); //기다리는 함수 , 다이얼로그 시스템이 완료 될때 까지 
         //인수는 대사 번호
     }
 }
