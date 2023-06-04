c001d5676207be0719ab7815bff1fa5771c78a5e
RyuHaJung <red061011@gmail.com>
2023-06-04 15:27:10 +0900

    이미지 업로드

---
+++ Assets/Scripts/Maze/Finish.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Finish : MonoBehaviour
+{
+    [SerializeField] Text finText;
+    [SerializeField] Button BackButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        finText.gameObject.SetActive(false);
+        BackButton.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        finText.gameObject.SetActive(true);
+        ItemSound.FinishSound();
+
+        BackButton.gameObject.SetActive(true);
+        ItemSound.FinishSound();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
