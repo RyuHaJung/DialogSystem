600eec71d68cafa2abfdcdae679c052e3cb1adbd
RyuHaJung <red061011@gmail.com>
2023-06-04 15:50:42 +0900

    미로 기본 세팅, 이미지 추가

---
+++ Assets/Scripts/Scene/SceneChanger.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void Change()
+    {
+        SceneManager.LoadScene("Text_003_Scene");
+    }
+
+    
+}
