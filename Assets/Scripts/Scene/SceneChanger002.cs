17082178d40473f20aecbb23e68734e6fdd19400
RyuHaJung <red061011@gmail.com>
2023-06-18 22:24:37 +0900

    시간 설정 추가

---
+++ Assets/Scripts/Scene/SceneChanger002.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger002 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Change()
+    {
+        SceneManager.LoadScene("Text_002_Scene");
+    }
+
+}
